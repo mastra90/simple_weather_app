124febf60034263e21c77a7957507c9aaa606cb9
mastra90 <lukejmastrocola@gmail.com>
2023-05-26 15:39:53 +1000

    App is functional
    When searching for a city, data is pulled from the API to display location, temp and humidity. Melbourne is loaded by default

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // JavaScript:
+updateWeatherFromApi ({location: "Melbourne"})
 
-// const btn = document.getElementById("submit-button");
 const btn = document.getElementById("form");
 btn.addEventListener("submit", async (e) => {
   e.preventDefault();
   const response = await fetchWeatherFromAPI(e.target[0].value);
+  updateWeatherInDom({...response, location:e.target[0].value});
   e.target[0].value = "";
-  updateWeatherInDom(response)
-  console.log(response);
 });
+
+async function updateWeatherFromApi ({location}) {
+    const response = await fetchWeatherFromAPI(location);
+    updateWeatherInDom({...response, location});
+}
 
 async function fetchWeatherFromAPI(location) {
     let url = `http://api.weatherapi.com/v1/current.json?key=6e2a12cd17ee48559d2120102232505&q=${encodeURIComponent(location)}`;
@@ -26,8 +30,16 @@ async function fetchWeatherFromAPI(location) {
 
 
 
-// {current.humidity: 62, current.temp_c: 13}
 
-function updateWeatherInDom({current:{humidity, temp_c}}) {
-    console.log(humidity, temp_c)
+function updateWeatherInDom({current:{humidity, temp_c}, location}) {
+    const destination = document.querySelector(".destination");
+    const degree = document.querySelector(".degree");
+    const humid = document.querySelector(".humid");
+
+    destination.innerHTML = location;
+    degree.innerHTML = `${temp_c}<span>°C</span>`;
+    humid.innerHTML = `${humidity}<em>%</em>`;
+
+
+    // console.log(location, humidity, temp_c)
 }
